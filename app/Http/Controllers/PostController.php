667bf5d72ee9f056ed36e7ad32b9ab80def2de25
mukhan350192 <mukhan350192@gmail.com>
2024-12-13 17:43:04 +0500

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,38 +14,42 @@ class PostController extends Controller
     {
         try {
             $post = $postService->createPost($request->user(), $request->all());
-            return response()->json(['success' => true,'data' => $post], 200);
+            return response()->json(['success' => true, 'data' => $post], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
 
-    public function getPosts(Request $request, PostService $postService){
+    public function getPosts(Request $request, PostService $postService)
+    {
         try {
             $posts = $postService->getPosts();
-            return response()->json(['success' => true,'data' => $posts], 200);
+            return response()->json(['success' => true, 'data' => $posts], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
 
-    public function getNearestPosts(NearPostRequest $request, PostService $postService){
+    public function getNearestPosts(NearPostRequest $request, PostService $postService)
+    {
         try {
-            $posts = $postService->getNearestPosts($request->latitude,$request->longitude);
-            return response()->json(['success' => true,'data' => $posts], 200);
+            $posts = $postService->getNearestPosts($request->latitude, $request->longitude);
+            return response()->json(['success' => true, 'data' => $posts], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
 
-    public function topPosts(Request $request){
-        return Post::withCount('rieltorComments')->with('user','getFirstImage')
-                ->orderBy('rieltor_comments_count','desc')
-                ->paginate(20);
+    public function topPosts(Request $request)
+    {
+        return response()->json(['success' => true, 'data' => Post::withCount('rieltorComments')->with('user', 'getFirstImage')
+            ->orderBy('rieltor_comments_count', 'desc')
+            ->paginate(20)]);
     }
 
-    public function getPostById($id){
-        return Post::with(['images','user','masterComments.user','rieltorComments.user'])->find($id);
+    public function getPostById($id)
+    {
+        return Post::with(['images', 'user', 'masterComments.user', 'rieltorComments.user'])->find($id);
     }
 
 }
